78ec85bb9a719f177e75475455bb67f0c957cffe
Emanuel Sanga <esanga530@gmail.com>
2019-09-10 01:14:16 +0300

    added an include for core application urls.py in the main urls.py file

--- jwtokenauth/urls.py
+++ jwtokenauth/urls.py
@@ -1,10 +1,10 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 from rest_framework_jwt.views import obtain_jwt_token
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-
-    path('token-auth/', obtain_jwt_token)
+    path('token-auth/', obtain_jwt_token),
+    path('core/', include('core.urls'))
 ]
