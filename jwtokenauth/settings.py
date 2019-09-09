cf84375458a99f2e4be5e781880a1f088610aeab
Emanuel Sanga <esanga530@gmail.com>
2019-09-10 01:21:43 +0300

    configuration done in settings to include the utils.py configurations of the jwt

--- jwtokenauth/settings.py
+++ jwtokenauth/settings.py
@@ -117,29 +117,22 @@ STATIC_URL = '/static/'
 # my own settings
 
 REST_FRAMEWORK = {
-
     'DEFAULT_PERMISSION_CLASSES': (
-
         'rest_framework.permissions.IsAuthenticated',
-
     ),
 
     'DEFAULT_AUTHENTICATION_CLASSES': (
-
         'rest_framework_jwt.authentication.JSONWebTokenAuthentication',
-
         'rest_framework.authentication.SessionAuthentication',
-
         'rest_framework.authentication.BasicAuthentication',
-
     ),
-
 }
 
-
-
 CORS_ORIGIN_WHITELIST = (
-
     'localhost:3000',
-
 )
+
+
+JWT_AUTH = {
+    'JWT_RESPONSE_PAYLOAD_HANDLER': 'jwtokenauth.utils.my_jwt_response_handler'
+}
