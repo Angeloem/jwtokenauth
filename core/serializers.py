c097fd34877c460b5da5c19eced8bda5fa5a6de7
Emanuel Sanga <esanga530@gmail.com>
2019-09-12 09:27:42 +0300

    pasted the userregistration serializer

--- core/serializers.py
+++ core/serializers.py
@@ -2,32 +2,61 @@ from rest_framework import serializers
 from rest_framework_jwt.settings import api_settings
 from django.contrib.auth.models import User
 
+#
+# class UserSerializer(serializers.ModelSerializer):
+#     class Meta:
+#         model = User
+#         fields = ('username',)
+#
+#
+# class UserSerializerWithToken(serializers.ModelSerializer):
+#     token = serializers.SerializerMethodField()
+#     password = serializers.CharField(write_only=True)
+#
+#     def get_token(self, obj):
+#         jwt_payload_handler = api_settings.JWT_PAYLOAD_HANDLER
+#         jwt_encode_handler = api_settings.JWT_ENCODE_HANDLER
+#         payload = jwt_payload_handler(obj)
+#         token = jwt_encode_handler(payload)
+#         return token
+#
+#     def create(self, validated_data):
+#         password = validated_data.pop('password', None)
+#         instance = self.Meta.model(**validated_data)
+#         if password is not None:
+#             instance.set_password(password)
+#         instance.save()
+#         return instance
+#
+#     class Meta:
+#         model = User
+#         fields = ('token', 'username', 'password')
 
-class UserSerializer(serializers.ModelSerializer):
+from rest_framework import serializers
+from .models import User
+
+
+class RegistrationSerializer(serializers.ModelSerializer):
+    """
+    Creates a new user.
+    Email, username, and password are required.
+    Returns a JSON web token.
+    """
+
+    # The password must be validated and should not be read by the client
+    password = serializers.CharField(
+        max_length=128,
+        min_length=8,
+        write_only=True,
+    )
+
+    # The client should not be able to send a token along with a registration
+    # request. Making `token` read-only handles that for us.
+    token = serializers.CharField(max_length=255, read_only=True)
+
     class Meta:
         model = User
-        fields = ('username',)
-
-
-class UserSerializerWithToken(serializers.ModelSerializer):
-    token = serializers.SerializerMethodField()
-    password = serializers.CharField(write_only=True)
-
-    def get_token(self, obj):
-        jwt_payload_handler = api_settings.JWT_PAYLOAD_HANDLER
-        jwt_encode_handler = api_settings.JWT_ENCODE_HANDLER
-        payload = jwt_payload_handler(obj)
-        token = jwt_encode_handler(payload)
-        return token
+        fields = ('email', 'username', 'password', 'token',)
 
     def create(self, validated_data):
-        password = validated_data.pop('password', None)
-        instance = self.Meta.model(**validated_data)
-        if password is not None:
-            instance.set_password(password)
-        instance.save()
-        return instance
-
-    class Meta:
-        model = User
-        fields = ('token', 'username', 'password')
+        return User.objects.create_user(**validated_data)
