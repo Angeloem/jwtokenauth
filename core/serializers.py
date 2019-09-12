843c790fbc36b3d5b650878a3e34c092853448a9
Emanuel Sanga <esanga530@gmail.com>
2019-09-12 09:35:59 +0300

    added an authenticate method to the serializers file

--- core/serializers.py
+++ core/serializers.py
@@ -1,3 +1,4 @@
+from django.contrib.auth import authenticate
 from rest_framework import serializers
 from rest_framework_jwt.settings import api_settings
 from django.contrib.auth.models import User
