04ce9dcadc8d4beb77d8ddf8a0d1f4fb392449af
Emanuel Sanga <esanga530@gmail.com>
2019-09-12 13:07:39 +0300

    added the views as well as the needed serializers

--- core/urls.py
+++ core/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
-from .views import current_user, UserList
+# from .views import current_user , UserList , LoginAPIView
+from core.views import LoginAPIView
 
 urlpatterns = [
-    path('current_user/', current_user),
-    path('users/', UserList.as_view())
+    # path('current_user/', current_user),
+    # path('users/', UserList.as_view()),
+    path('login', LoginAPIView.as_view())
+
 ]
--- core/views.py
+++ core/views.py
@@ -4,28 +4,82 @@ from rest_framework import permissions, status
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework.views import APIView
-from .serializers import UserSerializer, UserSerializerWithToken
+# from .serializers import UserSerializer, UserSerializerWithToken
+#
+#
+# @api_view(['GET'])
+# def current_user(request):
+#     """
+#     Determine the current user by their token, and return their data
+#     """
+#     serializer = UserSerializer(request.user)
+#     return Response(serializer.data)
+#
+#
+# class UserList(APIView):
+#     """
+#     Create a new user. It's called 'UserList' because normally we'd have a get
+#     method here too, for retrieving a list of all User objects.
+#     """
+#     permission_classes = (permissions.AllowAny,)
+#
+#     def post(self, request, format=None):
+#         serializer = UserSerializerWithToken(data = request.data)
+#         if serializer.is_valid():
+#             serializer.save()
+#             return Response(serializer.data, status = status.HTTP_201_CREATED)
+#         return Response(serializer.errors, status = status.HTTP_400_BAD_REQUEST)
 
 
-@api_view(['GET'])
-def current_user(request):
+from rest_framework import status
+from rest_framework.permissions import AllowAny
+from rest_framework.response import Response
+from rest_framework.views import APIView
+
+from .models import User
+from .serializers import LoginSerializer
+from .serializers import RegistrationSerializer
+
+
+class RegistrationAPIView(APIView):
     """
-    Determine the current user by their token, and return their data
+    Registers a new user.
     """
-    serializer = UserSerializer(request.user)
-    return Response(serializer.data)
+    permission_classes = [AllowAny]
+    serializer_class = RegistrationSerializer
+
+    def post(self, request):
+        """
+        Creates a new User object.
+        Username, email, and password are required.
+        Returns a JSON web token.
+        """
+        serializer = self.serializer_class(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+
+        return Response(
+            {
+                'token': serializer.data.get('token', None),
+            },
+            status=status.HTTP_201_CREATED,
+        )
 
 
-class UserList(APIView):
+class LoginAPIView(APIView):
     """
-    Create a new user. It's called 'UserList' because normally we'd have a get
-    method here too, for retrieving a list of all User objects.
+    Logs in an existing user.
     """
-    permission_classes = (permissions.AllowAny,)
+    permission_classes = [AllowAny]
+    serializer_class = LoginSerializer
 
-    def post(self, request, format=None):
-        serializer = UserSerializerWithToken(data = request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status = status.HTTP_201_CREATED)
-        return Response(serializer.errors, status = status.HTTP_400_BAD_REQUEST)
+    def post(self, request):
+        """
+        Checks is user exists.
+        Email and password are required.
+        Returns a JSON web token.
+        """
+        serializer = self.serializer_class(data=request.data)
+        serializer.is_valid(raise_exception=True)
+
+        return Response(serializer.data, status=status.HTTP_200_OK)
